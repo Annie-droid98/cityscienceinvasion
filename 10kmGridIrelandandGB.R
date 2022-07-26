c6b21c8e2a9fd64cb2633baeb9dce8d9492b3b3c
Annie-droid98 <annie-harke@gmx.de>
2022-07-26 08:48:40 +0200

    needed grids for the analysis

---
+++ 10kmGridIrelandandGB.R
@@ -0,0 +1,36 @@
+library(dplyr)
+library(sf)
+library(rgdal)
+library(raster)
+
+download.file("https://www.eea.europa.eu/data-and-maps/data/eea-reference-grids-2/gis-files/europe-10-km-100-km/at_download/file", destfile = file.path(tempdir(), "Europe.zip"), mode = "wb")
+unzip(zipfile = file.path(tempdir(), "Europe.zip"), exdir = tempdir())
+Europe10grid <- shapefile(file.path(tempdir(), "europe_10km.shp"))
+Europe10grid  <- st_as_sf(Europe10grid)
+download.file("https://www.eea.europa.eu/data-and-maps/data/eea-reference-grids-2/gis-files/ireland-shapefile/at_download/file",
+              destfile = file.path(tempdir(), "Ireland_shapefile"), mode = "wb")
+unzip(zipfile = file.path(tempdir(), "Ireland_shapefile"), exdir = tempdir())  
+
+Ireland_10grid <- shapefile(file.path(tempdir(), "ie_10km.shp"))
+
+download.file("https://www.eea.europa.eu/data-and-maps/data/eea-reference-grids-2/gis-files/great-britain-shapefile/at_download/file",
+              destfile = file.path(tempdir(), "Great_Britain_shapefile"), mode = "wb")
+unzip(zipfile = file.path(tempdir(), "Great_Britain_shapefile"), exdir = tempdir())  
+
+GB_10grid <- shapefile(file.path(tempdir(), "gb_10km.shp"))
+
+GB_and_IE_grid_10km <- bind(Ireland_10grid, GB_10grid )%>%
+  st_as_sf()
+Grid_ohneduplices <- GB_and_IE_grid_10km%>%
+  distinct()
+
+#as shapefile
+GB_and_IE_grid_10km_shp <- bind(Ireland_10grid, GB_10grid)
+
+png(filename= "MapGrids2.png")
+plot(Grid_ohneduplices$geometry)
+dev.off()
+
+png(filename= "MapGrids3.png")
+plot(GB_and_IE_grid_10km$geometry)
+dev.off()
